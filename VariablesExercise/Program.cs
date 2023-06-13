50bfa0e10457856976edeb0ee75c321a0da1752e
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-06-13 16:39:04 -0400

    Completed Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Declare and Initialize variables
+            string favoriteColor = "Blue";
+            int age = 30;
+            char bloodType = 'O';
+            bool hasDriverLicense = true;
+            double salary = 50500.00;
+            decimal bankBalance = 10000.75m;
+
+            Console.WriteLine($"Hello, World! The person's favorite color is {favoriteColor} and they are {age} years old. Their blood type is {bloodType} and it's {hasDriverLicense} they have a driver's license. They make ${salary} a year and have a bank balance of ${bankBalance}.");
         }
     }
 }
